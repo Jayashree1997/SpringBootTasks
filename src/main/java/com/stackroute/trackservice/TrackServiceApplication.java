af7f88caf18417022c35e424a7e5e6d82d5fa005
Jayashree <jayashree.a@cgi.com>
2019-08-02 14:08:09 +0530

    Swagger code updated

--- src/main/java/com/stackroute/trackservice/TrackServiceApplication.java
+++ src/main/java/com/stackroute/trackservice/TrackServiceApplication.java
@@ -1,8 +1,10 @@
 package com.stackroute.trackservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class TrackServiceApplication {
 
 	public static void main(String[] args) {
