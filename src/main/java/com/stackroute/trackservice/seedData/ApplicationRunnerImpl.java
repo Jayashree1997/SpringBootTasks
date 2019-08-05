34374b59f8ac26ef35d201c3c2de1bddccad299c
Jayashree <jayashree.a@cgi.com>
2019-08-05 12:22:35 +0530

    task11 code updated

--- src/main/java/com/stackroute/trackservice/seedData/ApplicationRunnerImpl.java
+++ src/main/java/com/stackroute/trackservice/seedData/ApplicationRunnerImpl.java
@@ -3,6 +3,7 @@ package com.stackroute.trackservice.seedData;
 import com.stackroute.trackservice.domain.Track;
 import com.stackroute.trackservice.repository.TrackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationEvent;
@@ -12,6 +13,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicationRunnerImpl  implements ApplicationListener {
+    @Value("${track.id}")
+    private int id;
+    @Value("${track.name}")
+    private String name;
+    @Value("${track.comments}")
+    private String comments;
+    Track track=new Track();
     private TrackRepository trackRepository;
 
     @Autowired
@@ -22,12 +30,16 @@ public class ApplicationRunnerImpl  implements ApplicationListener {
 
     @Override
     public void onApplicationEvent(ApplicationEvent applicationEvent) {
-        Track track1=new Track(78,"abc","xyz");
-        trackRepository.save(track1);
-        Track track2=new Track(79,"gbh","klk");
-        trackRepository.save(track2);
-        Track track3=new Track(80,"mnb","vbc");
-        trackRepository.save(track3);
+        track.setId(id);
+        track.setName(name);
+        track.setComments(comments);
+        trackRepository.save(track);
+//        Track track1=new Track(78,"abc","xyz");
+//        trackRepository.save(track1);
+//        Track track2=new Track(79,"gbh","klk");
+//        trackRepository.save(track2);
+//        Track track3=new Track(80,"mnb","vbc");
+//        trackRepository.save(track3);
 
     }
 }
